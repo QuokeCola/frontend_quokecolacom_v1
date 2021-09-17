b835901f80099fa0c433a94d08c3f2737590569e
quokecola <QuokeCola@iCloud.com>
2021-09-17 00:43:23 -0500

    Optimized the structure

--- _scripts/navigationbar.js
+++ _scripts/navigationbar.js
@@ -1,15 +1,17 @@
 // JavaScript Document
 document.write("<script language=javascript src='./_scripts/isPC.js'></script>");
-var navi_title = document.getElementById("navi_title_obj");
-var navi_logo_line = document.getElementById("navi_titleline_obj");
-var navi_logo_union = document.getElementById("navi_title_union");
-var navi_background = document.getElementById("navi_background_obj");
+var navi_title 				= document.getElementById("navi_title_obj");
+var navi_logo_line 			= document.getElementById("navi_titleline_obj");
+var navi_logo_union 		= document.getElementById("navi_title_union");
+var navi_background 		= document.getElementById("navi_background_obj");
+var navi_enable_expanded 	= document.getElementById("navi_enable_expanded");
+navi_enable_expanded.checked= 1;
 var navi_mobile_menu_button = document.getElementById("navi_mobile_menu_button");
-var navi_mobile_menu_state = document.getElementById("navi_mobile_menu_state_obj");
-var navi_pc_page_button_background = document.getElementById("navi_pc_page_button_background");
-var navi_pc_decoration = document.getElementById("navi_pc_decoration_obj");
-var navi_pc_strip = document.getElementById("navi_pc_strip");
-var navi_pc_page_button_background_width = 0;
+var navi_mobile_menu_state 	= document.getElementById("navi_mobile_menu_state_obj");
+var navi_pc_decoration 		= document.getElementById("navi_pc_decoration_obj");
+var navi_pc_strip 			= document.getElementById("navi_pc_strip");
+var navi_pc_page_button_background 			= document.getElementById("navi_pc_page_button_background");
+var navi_pc_page_button_background_width 	= 0;
 
 if(isPC()) {
 	navi_mobile_menu_button.style.display = "none";
@@ -23,16 +25,18 @@ if(isPC()) {
 
 var previousY = window.pageYOffset;
 window.onscroll = function(e) {
-	var currentY = window.pageYOffset;
-	var diff = currentY - previousY;
-	if(currentY > 0 && !navi_mobile_menu_state.checked) {
-		set_title_bar_collapse();
-		set_title_bar_colored();
-	} else if (diff < 0  && !navi_mobile_menu_state.checked){
-		set_title_bar_transparent();
-		set_title_bar_expanded();
+	if(navi_enable_expanded.checked) {
+		var currentY = window.pageYOffset;
+		var diff = currentY - previousY;
+		if(currentY > 0) {
+			set_title_bar_collapse();
+			set_title_bar_colored();
+		} else if (diff < 0){
+			set_title_bar_transparent();
+				set_title_bar_expanded();
+		}
+		previousY = currentY;
 	}
-	previousY = currentY;
 }
 
 function navi_initiate(){
@@ -40,37 +44,55 @@ function navi_initiate(){
 }
 
 function set_title_bar_colored() {
+	// PC navigation decoration "</>"
 	navi_pc_decoration.style.backgroundColor="rgba(255,255,255,0.0)";
 	navi_pc_decoration.style['-webkit-backdrop-filter']='blur(0px)';
 	navi_pc_decoration.style['backdrop-filter']='blur(0px)';
 	navi_pc_decoration.style.color="white";
 	navi_pc_decoration.style['backdrop-filter']='blur(0px)';
+	
+	// Navigation Background
 	navi_background.style['-webkit-backdrop-filter']='blur(10px)';
 	navi_background.style['backdrop-filter']='blur(20px)';
 	navi_background.style.backgroundColor = "rgba(12, 40, 82, 0.9)";
+	
+	// Navigation button background
 	navi_pc_page_button_background.style.width=String(screen.width)+"px";
 	navi_pc_page_button_background.style.borderRadius = "0px";
 	navi_pc_page_button_background.style['-webkit-backdrop-filter']='blur(0px)';
 	navi_pc_page_button_background.style['backdrop-filter']='blur(0px)';
+	
+	// Change color for navigation title.
 	navi_title.style.backgroundColor="rgba(0,0,0,0.0)";
+	
+	// Change color for navigation title background.
 	navi_logo_union.style['-webkit-backdrop-filter']='blur(0px)';
 	navi_logo_union.style['backdrop-filter']='blur(0px)';
 	navi_logo_union.style.backgroundColor="rgba(0,0,0,0.0)";
 }
 
 function set_title_bar_transparent() {
+	// PC navigation decoration "</>"
 	navi_pc_decoration.style.backgroundColor="rgba(255,255,255,0.9)";
 	navi_pc_decoration.style['-webkit-backdrop-filter']='blur(10px)';
 	navi_pc_decoration.style['backdrop-filter']='blur(20px)';
 	navi_pc_decoration.style.color="black";
+	
+	// Navigation Background
 	navi_background.style['-webkit-backdrop-filter']="blur(0px)";
 	navi_background.style['backdrop-filter']="blur(0px)";
 	navi_background.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
+	
+	// Navigation button background
 	navi_pc_page_button_background.style.width=navi_pc_page_button_background_width+"px";
 	navi_pc_page_button_background.style.borderRadius = "7px";
 	navi_pc_page_button_background.style['-webkit-backdrop-filter']='blur(10px)';
 	navi_pc_page_button_background.style['backdrop-filter']='blur(20px)';
+	
+	// Change color for navigation title.
 	navi_title.style.backgroundColor="rgba(255,255,255,0.0)";
+	
+	// Change color for navigation title background.
 	navi_logo_union.style["-webkit-backdrop-filter"]='blur(10px)';
 	navi_logo_union.style['backdrop-filter']='blur(20px)';
 	navi_logo_union.style.backgroundColor="rgb(12, 40, 82, 0.9)";
@@ -89,18 +111,17 @@ function set_title_bar_expanded() {
 }
 
 function navi_mobile_menu_button_clk() {
-	if(navi_mobile_menu_state.checked) {
-		if(window.pageYOffset > 0){
+	if(navi_mobile_menu_state.checked) {	// Click to uncheck
+		if(window.pageYOffset>0) {
 			set_title_bar_collapse();
 		} else {
 			set_title_bar_transparent();
 		}
-	} else {
-		if(window.pageYOffset > 0) {
-			set_title_bar_expanded();
-		} else {
-			set_title_bar_colored();
-		}
+		navi_enable_expanded.checked = 1;
+	} else {								// Click to check
+		set_title_bar_expanded();
+		set_title_bar_colored();
+		navi_enable_expanded.checked = 0;
 	}
 }
 
