d95bf9af667c22a1bb08926465065a661cbc2b05
quokecola <quokecola@icloud.com>
2021-12-16 04:58:46 -0600

    Adjust README.md

--- README.md
+++ README.md
@@ -3,7 +3,3 @@ A personal website with self designed html, css and javascript
 Some of the resources are just for backup.
 
 Please view the website by www.quokecola.com
-
-# !Important Notice
-Reloading iFrame will cause memory leak. The website will be temporary shutdown.
-Instead of using iFrame, Ajax mode will be adopted, but it will take few days to refactor the code.
--- comp/nav/scripts/script.js
+++ comp/nav/scripts/script.js
@@ -1,36 +1,41 @@
-document.write("<script language=javascript src='/_scripts/common_macro.js'></script>");
-
 class NavViewController{
-    _nav_json_url = "/comp/nav/customize.json";
     _customized_json;
     _scroll_shrink_threshold = 5;
     _previousLayoutID = 0;
 
-    obj_top_strip = document.getElementById("nav-topStrip-obj");
+    _enable_expand = true;
+
+    obj_nav       = document.getElementById("nav");
     obj_button_box = document.getElementById("nav-buttonBox-obj");
-    obj_mobile_menu_state = document.getElementById("nav-mobileMenuState");
     obj_pc_shrink_state = document.getElementById("nav-pcShrinkState");
+    obj_content_container = document.getElementById("content-container");
 
     style_button_box_pc_content_width;
 
-    constructor() {
+    constructor(customize_json) {
         let _thisRef = this;
-        let json = new XMLHttpRequest();
-        json.open("get", this._nav_json_url);
-        json.send(null);
-        json.onload = function (){
-            if (json.status === 200) {
-                _thisRef._customized_json = JSON.parse(json.responseText);
-                _thisRef.initialize_UI();
-            }
-        }
+        this._customized_json = customize_json;
+        this.initialize_UI();
         window.addEventListener('resize', function (){
             _thisRef.resize();
         });
 
-        window.addEventListener('scroll', function () {
-            _thisRef.scroll();
+        window.addEventListener("orientationchange", function () {
+            _thisRef.refresh_UI();
         })
+
+        window.addEventListener("toggleNavShrinkRequest", evt => {
+            _thisRef.enable_expand(evt.detail.value);
+        })
+
+        this.obj_content_container.addEventListener('scroll',function () {
+            _thisRef.scroll();
+        });
+    }
+
+    enable_expand(value) {
+        this._enable_expand = value;
+        this.scroll();
     }
 
     resize() {
@@ -45,7 +50,7 @@ class NavViewController{
     }
 
     scroll() {
-        this.obj_pc_shrink_state.checked = document.documentElement.scrollTop < this._scroll_shrink_threshold;
+        this.obj_pc_shrink_state.checked = this.obj_content_container.scrollTop < this._scroll_shrink_threshold && this._enable_expand;
     }
 
     initialize_UI() {
@@ -55,6 +60,12 @@ class NavViewController{
             for(let i = 0; i < this._customized_json.subpages.length; i++){
                 let button = document.createElement("button");
                 button.innerText = this._customized_json.subpages[i].title;
+                button.onclick = function () {
+                    let Event = new CustomEvent("updateCCRequest", {
+                        detail:{src: _thisRef._customized_json.subpages[i]}
+                    });
+                    window.dispatchEvent(Event);
+                }
                 this.obj_button_box.appendChild(button);
             }
             document.documentElement.style.setProperty("--theme-color",
@@ -65,6 +76,8 @@ class NavViewController{
                 this._customized_json.complementaryColor);
             setTimeout(function (){
                 _thisRef.style_button_box_pc_content_width = _thisRef._get_pc_button_box_inner_width()+"px";
+                document.documentElement.style.setProperty("--pc-buttonBox-width-shrink",
+                    _thisRef.style_button_box_pc_content_width);
                 _thisRef.refresh_UI();
             },20);
 
@@ -77,14 +90,10 @@ class NavViewController{
         document.documentElement.style.setProperty("--pc-buttonBox-width-expand",
             window.innerWidth+"px");
         if(getLayoutID()===2){
-            this.obj_button_box.classList.replace("nav-mobileButtonBox","nav-pcButtonBox");
-            this.obj_top_strip.classList.replace("nav-mobileTopStrip", "nav-pcTopStrip" );
-            document.documentElement.style.setProperty("--pc-buttonBox-width-shrink",
-                this.style_button_box_pc_content_width);
-            this.obj_pc_shrink_state.checked = document.documentElement.scrollTop < this._scroll_shrink_threshold;
+            this.obj_nav.classList.replace("nav-mobile", "nav-pc");
+            this.scroll();
         } else {
-            this.obj_button_box.classList.replace("nav-pcButtonBox", "nav-mobileButtonBox");
-            this.obj_top_strip.classList.replace("nav-pcTopStrip","nav-mobileTopStrip" );
+            this.obj_nav.classList.replace( "nav-pc","nav-mobile");
         }
     }
 
@@ -93,7 +102,6 @@ class NavViewController{
         for (let i = 0; i < this.obj_button_box.children.length; i++){
             let bound = this.obj_button_box.children[i].getBoundingClientRect();
             width = width+bound.width;
-            console.log(width);
         }
         return width;
     }
